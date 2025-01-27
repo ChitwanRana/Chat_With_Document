f5be13d4a0f5cf80aa9cea6b8b50ebdf0b139fab
ChitwanRana <kushr4446@gmail.com>
2025-01-27 18:59:49 +0530

    Demo

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
-import os
 import streamlit as st
+import os
 from langchain_groq import ChatGroq
-from langchain_text_splitters import RecursiveCharacterTextSplitter # to convert document into chunks
-from langchain.chains.combine_documents import create_stuff_documents_chain # to get relevent data from document 
-from langchain.chains import create_retrieval_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.vectorstores import FAISS  # vector db 
-from langchain_community.document_loaders import PyPDFDirectoryLoader    # to read document 
-from langchain_google_genai import GoogleGenerativeAIEmbeddings     # embedding technique 
-
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
-load_dotenv()  # to load environment variable 
+import os
+load_dotenv()
 
-# load groq and google API 
+## load the GROQ And OpenAI API KEY 
+groq_api_key=os.getenv('GROQ_API_KEY')
+os.environ["GOOGLE_API_KEY"]=os.getenv("GOOGLE_API_KEY")
 
-groq_api_key=os.getenv("GROQ_API_KEY")
-os.environ['GOOGLE_API_KEY']=os.getenv('GOOGLE_API_KEY')
+st.title("Gemma Model Document Q&A")
 
-
-st.title("Chat_With_Document")
-
-# load model
-llm=ChatGroq(groq_api_key=groq_api_key , model_name="Llama3-8b-8192")
+llm=ChatGroq(groq_api_key=groq_api_key,
+             model_name="Llama3-8b-8192")
 
 prompt=ChatPromptTemplate.from_template(
 """
@@ -35,22 +33,31 @@ Questions:{input}
 """
 )
 
-def vector_embedding():       # save entire document into session state so that we can use it anywhere 
-     if "vectors" not in st.session_state:
-          st.session_state.embeddings=GoogleGenerativeAIEmbeddings()
-          st.session_state.loader=PyPDFDirectoryLoader("./PDFs")  #data ingestion 
-          st.session_state.docs=st.session_state.loader.load()# document loading 
-          st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) 
-          st.session_state.final_document=st.session_state.text_splitter.split_documents(st.session_state.docs)
-          st.session_state.vectors=FAISS.from_documents(st.session_state.final_document,st.session_state.embeddings)
+def vector_embedding():
 
-prompt1 = st.text_input("Give prompt ")
+    if "vectors" not in st.session_state:
 
-if st.button("create vector embeddings"):
-     vector_embedding()
-     st.write("vector db is ready ")
+        st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+        st.session_state.loader=PyPDFDirectoryLoader("./PDFs") ## Data Ingestion
+        st.session_state.docs=st.session_state.loader.load() ## Document Loading
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) ## Chunk Creation
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20]) #splitting
+        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings) #vector OpenAI embeddings
 
-import time 
+
+
+
+
+prompt1=st.text_input("Enter Your Question From Doduments")
+
+
+if st.button("Documents Embedding"):
+    vector_embedding()
+    st.write("Vector Store DB Is Ready")
+
+import time
+
+
 
 if prompt1:
     document_chain=create_stuff_documents_chain(llm,prompt)
@@ -66,5 +73,4 @@ if prompt1:
         # Find the relevant chunks
         for i, doc in enumerate(response["context"]):
             st.write(doc.page_content)
-            st.write("--------------------------------")    
-
+            st.write("--------------------------------")
